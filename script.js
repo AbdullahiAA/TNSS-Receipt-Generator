224b884ffa51d293a489ffe1a31b7a72b63c6961
Abdullah <osuolaleabdullahi@gmail.com>
2021-08-14 00:31:50 +0100

    Amount Field Added

--- script.js
+++ script.js
@@ -1,21 +1,49 @@
 const form = document.querySelector("form");
 const receipt = document.querySelector(".app__receipt");
+const refreshBtnn = document.querySelector(".refreshBtn");
 
 form.addEventListener("submit", (e) => {
    e.preventDefault();
-   const s_name = e.target.name.value;
+   var s_name = e.target.name.value;
    const s_class = e.target.class.value;
+   const amount = e.target.amount.value;
 
    console.log("Name", s_name);
    console.log("Class", s_class);
+   console.log("Amount", amount);
 
    //    Hide the form
    form.style.display = "none";
 
    const printBtn =
       "<input type='button' value='Print' class='btn printBtn' onclick='window.print()' />";
+   const refreshBtn =
+      "<input type='button' value='Refresh' class='btn refreshBtn' onclick='refreshPage()' />";
 
    //    Show the Receipt
    receipt.innerHTML =
-      "<h3>" + s_name + "</h3><h3>" + s_class + "</h3>" + printBtn;
+      "<h6>Name: " +
+      s_name +
+      "</h6><h6>Class: " +
+      s_class +
+      "</h6>" +
+      "<h6>Amount Paid: " +
+      amount +
+      "</h6>" +
+      printBtn +
+      refreshBtn;
+
+   receipt.style.display = "block";
+
+   e.target.name.value = "";
+   e.target.class.value = "";
+   e.target.amount.value = "";
 });
+
+function refreshPage() {
+   //    Show the form
+   form.style.display = "block";
+
+   //    Hide the Receipt
+   receipt.style.display = "none";
+}
