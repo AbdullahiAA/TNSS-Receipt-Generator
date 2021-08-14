71e15a411628a12a7beec717f2f2ef3ce1aa5f42
Abdullah <osuolaleabdullahi@gmail.com>
2021-08-14 23:21:02 +0100

    Semi-Final Stage

--- script.js
+++ script.js
@@ -1,47 +1,53 @@
 const form = document.querySelector("form");
+
 const receipt = document.querySelector(".app__receipt");
-const refreshBtnn = document.querySelector(".refreshBtn");
+const receiptBody = document.querySelector(".app__receipt--body");
+
+const receiptIDField = document.querySelector(".receiptID");
+const transDateField = document.querySelector(".transDate");
+const stdNameField = document.querySelector(".stdName");
+const classField = document.querySelector(".class");
+const amountField = document.querySelector(".amount");
+const paymentForField = document.querySelector(".paymentFor");
+const PayedByFields = document.querySelectorAll(".PayedBy");
 
 form.addEventListener("submit", (e) => {
    e.preventDefault();
-   var s_name = e.target.name.value;
-   const s_class = e.target.class.value;
+
+   const day = getTime().dayName;
+   const date = getTime().date;
+   const time = getTime().time;
+
+   const transDate = `${day}, ${date} ${time}`;
+
+   const stdName = e.target.name.value;
+   const stdClass = e.target.class.value;
    const amount = e.target.amount.value;
    const purpose = e.target.purpose.value;
+   const depositor = e.target.depositor.value;
 
-   console.log("Name", s_name);
-   console.log("Class", s_class);
-   console.log("Amount", amount);
-   console.log("Purpose", purpose);
-
-   //    Hide the form
+   //    Hide the form...
    form.style.display = "none";
 
-   const printBtn =
-      "<input type='button' value='Print' class='btn printBtn' onclick='window.print()' />";
-   const refreshBtn =
-      "<input type='button' value='Refresh' class='btn refreshBtn' onclick='refreshPage()' />";
+   //    Fill the receipt with the data
+   receiptIDField.innerHTML = generateID();
+   transDateField.innerHTML = transDate;
+   stdNameField.innerHTML = stdName;
+   classField.innerHTML = stdClass;
+   amountField.innerHTML = amount;
+   paymentForField.innerHTML = purpose;
+   PayedByFields[0].innerHTML = depositor;
+   PayedByFields[1].innerHTML = depositor;
 
-   //    Show the Receipt
-   receipt.innerHTML =
-      "<h6>Name: " +
-      s_name +
-      "</h6><h6>Class: " +
-      s_class +
-      "</h6><h6>Amount Paid: " +
-      amount +
-      "</h6><h6>Payment for: " +
-      purpose +
-      "</h6>" +
-      printBtn +
-      refreshBtn;
-
+   //   Show the receipt...
    receipt.style.display = "block";
 
+   //    Reset the form fields...
    e.target.name.value = "";
    e.target.class.value = "";
    e.target.amount.value = "";
-   e.target.purpose.value = "";
+   e.target.purpose.value = "School Fee";
+   e.target.depositor.value = "";
 });
 
 function refreshPage() {
@@ -50,4 +56,76 @@ function refreshPage() {
 
    //    Hide the Receipt
    receipt.style.display = "none";
+}
+
+function getTime() {
+   let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+
+   let months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sept",
+      "Oct",
+      "Nov",
+      "Dec",
+   ];
+
+   let date = new Date();
+
+   let dayName = days[date.getDay()];
+
+   let day = date.getDate();
+   let month = months[date.getMonth()];
+   let year = date.getFullYear();
+
+   let hour = date.getHours();
+   let __hour = hour;
+   if (hour > 12) __hour = hour - 12;
+
+   let minute = date.getMinutes();
+   if (minute < 10) minute = "0" + minute;
+
+   let meridian = "am";
+   if (hour >= 12) meridian = "pm";
+
+   return {
+      dayName: dayName,
+      date: `${day} ${month} ${year}`,
+      time: `${__hour}:${minute}${meridian}`,
+   };
+}
+
+function generateID() {
+   const date = new Date();
+
+   const day = date.getDate();
+   const month = date.getMonth();
+
+   const transCount = getTransCount();
+
+   const ID = "SR" + day + `${month + 1}` + transCount;
+
+   return ID;
+}
+
+function getTransCount() {
+   let transCount = localStorage.getItem("transCount") || 0;
+
+   transCount++;
+
+   localStorage.setItem("transCount", transCount);
+
+   if (transCount < 10) {
+      transCount = "00" + transCount;
+   } else if (transCount < 100) {
+      transCount = "0" + transCount;
+   }
+
+   return transCount;
 }
