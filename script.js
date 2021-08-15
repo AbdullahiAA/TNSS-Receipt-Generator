8f48d0a098c3916f64c23c4970d8389e5be2b219
Abdullah <osuolaleabdullahi@gmail.com>
2021-08-15 13:30:09 +0100

    Transaction category implemented

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const form = document.querySelector("form");
 
 const receipt = document.querySelector(".app__receipt");
-const receiptBody = document.querySelector(".app__receipt--body");
 
+const transCategory = document.getElementsByName("category");
+const classOptions = document.getElementById("class");
+
+const schNameField = document.querySelector(".app__receipt--schName");
 const receiptIDField = document.querySelector(".receiptID");
 const transDateField = document.querySelector(".transDate");
 const stdNameField = document.querySelector(".stdName");
@@ -11,30 +14,84 @@ const amountField = document.querySelector(".amount");
 const paymentForField = document.querySelector(".paymentFor");
 const PayedByFields = document.querySelectorAll(".PayedBy");
 
+const madrasahClasses = [
+   "Pre-Nur",
+   "NUR 1",
+   "NUR 2",
+   "PRY 1",
+   "PRY 2",
+   "PRY 3",
+   "PRY 4",
+   "JSS 1",
+   "JSS 2",
+   "JSS 3",
+   "SSS 1",
+   "SSS 2",
+   "SSS 3",
+];
+
+const tnssClasses = [
+   "Creche",
+   "K.G 1",
+   "K.G 2",
+   "NUR 1",
+   "NUR 2",
+   "PRY 1",
+   "PRY 2",
+   "PRY 3",
+   "PRY 4",
+   "PRY 5",
+   "JSS 1",
+   "JSS 2",
+   "JSS 3",
+   "SSS 1",
+   "SSS 2",
+   "SSS 3",
+];
+
+// Handle Transaction Category Change...
+transCategory.forEach((category) => {
+   category.addEventListener("change", () => {
+      const value = category.value;
+
+      handleCategoryChange(value);
+   });
+});
+
+// Handle Form Submission...
 form.addEventListener("submit", (e) => {
    e.preventDefault();
 
+   //    Get the transaction date...
    const day = getTime().dayName;
    const date = getTime().date;
    const time = getTime().time;
 
    const transDate = `${day}, ${date} ${time}`;
 
+   //    Get the data from the form...
    const stdName = e.target.name.value;
    const stdClass = e.target.class.value;
    const amount = e.target.amount.value;
    const purpose = e.target.purpose.value;
    const depositor = e.target.depositor.value;
 
+   //    Format the amount into Naira...
+   const formattedAmount = new Intl.NumberFormat("en-NG", {
+      style: "currency",
+      currency: "NGN",
+      minimumFractionDigits: 0,
+   }).format(amount);
+
    //    Hide the form...
    form.style.display = "none";
 
-   //    Fill the receipt with the data
+   //    Fill the receipt with the data...
    receiptIDField.innerHTML = generateID();
    transDateField.innerHTML = transDate;
    stdNameField.innerHTML = stdName;
    classField.innerHTML = stdClass;
-   amountField.innerHTML = amount;
+   amountField.innerHTML = formattedAmount;
    paymentForField.innerHTML = purpose;
    PayedByFields[0].innerHTML = depositor;
    PayedByFields[1].innerHTML = depositor;
@@ -48,6 +105,7 @@ form.addEventListener("submit", (e) => {
    e.target.amount.value = "";
    e.target.purpose.value = "School Fee";
    e.target.depositor.value = "";
+   transCategory[0].checked = true;
 });
 
 function refreshPage() {
@@ -56,6 +114,8 @@ function refreshPage() {
 
    //    Hide the Receipt
    receipt.style.display = "none";
+
+   handleCategoryChange("The Noble");
 }
 
 function getTime() {
@@ -129,4 +189,29 @@ function getTransCount() {
    }
 
    return transCount;
+}
+
+function handleCategoryChange(value) {
+   let classes = madrasahClasses;
+
+   if (value === "Nujabai") {
+      schNameField.innerHTML =
+         "معهد النجباء لتعليم اللغة العربية والعلوم الشرعية";
+   } else {
+      schNameField.innerHTML = "THE NOBLE STANDARD SCHOOLS";
+      classes = tnssClasses;
+   }
+
+   //   Setting the first option to "Choose Class"
+   classOptions.innerHTML =
+      '<option value="" disabled hidden selected>Choose Class</option>';
+
+   // Showing the Classes options
+   classes.forEach((classItem) => {
+      const newOption = document.createElement("OPTION");
+      const textNode = document.createTextNode(classItem);
+      newOption.appendChild(textNode);
+
+      classOptions.append(newOption);
+   });
 }
