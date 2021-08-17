b96f2dafa1aa5b6f91b659f86628b316045211fc
Abdullah <osuolaleabdullahi@gmail.com>
2021-08-17 09:53:36 +0100

    Payment For UI Updated

--- sw.js
+++ sw.js
@@ -1,29 +1,29 @@
-self.addEventListener("install", (e) => {
-   e.waitUntil(
-      caches
-         .open("TNSS-receipt-generator")
-         .then((cache) =>
-            cache.addAll([
-               "/",
-               "/index.html",
+// self.addEventListener("install", (e) => {
+//    e.waitUntil(
+//       caches
+//          .open("TNSS-receipt-generator")
+//          .then((cache) =>
+//             cache.addAll([
+//                "/",
+//                "/index.html",
 
-               "/script/headerBtns.js",
-               "/script/index.js",
-               "/script/login.js",
-               "/script/register.js",
-               "/script/settings.js",
-               "/script/transaction.js",
+//                "/script/headerBtns.js",
+//                "/script/index.js",
+//                "/script/login.js",
+//                "/script/register.js",
+//                "/script/settings.js",
+//                "/script/transaction.js",
 
-               "/css/styles.css",
-               "/icon/icon.jpg",
-            ])
-         )
-   );
-});
+//                "/css/styles.css",
+//                "/icon/icon.jpg",
+//             ])
+//          )
+//    );
+// });
 
-self.addEventListener("fetch", (e) => {
-   // console.log(e.request.url);
-   e.respondWith(
-      caches.match(e.request).then((response) => response || fetch(e.request))
-   );
-});
+// self.addEventListener("fetch", (e) => {
+//    // console.log(e.request.url);
+//    e.respondWith(
+//       caches.match(e.request).then((response) => response || fetch(e.request))
+//    );
+// });
